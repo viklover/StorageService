45030e4780e1fe90b2bb5b60825d759b8b4c3320
Mihail Vorobyev <1viklover@gmail.com>
2024-01-15 00:59:14 +0500

    [FIX] cassandra auth via env variables

--- src/Repository/Storage/StorageRepository.cs
+++ src/Repository/Storage/StorageRepository.cs
@@ -6,14 +6,18 @@ namespace Repository.Storage;
 public class StorageRepository : IStorageRepository
 {
     private readonly Cluster _cluster = Cluster.Builder()
-        .AddContactPoint(Environment.GetEnvironmentVariable("CLUSTER_URL"))
+        .AddContactPoint(Environment.GetEnvironmentVariable("CASSANDRA_HOST"))
+        .WithAuthProvider(
+            new PlainTextAuthProvider(
+                Environment.GetEnvironmentVariable("CASSANDRA_USER"),
+                Environment.GetEnvironmentVariable("CASSANDRA_PASSWORD")))
         .Build();
 
     public ISession Session() => _cluster.Connect("storage");
 
     public void PrepareSchemas()
     {
-        throw new NotImplementedException();
+        // throw new NotImplementedException();
     }
 
     ~StorageRepository()
