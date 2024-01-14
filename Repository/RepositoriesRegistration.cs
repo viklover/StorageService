3fd16c11f91a6f2bb1a01a463c57cf015485b5a6
Mihail Vorobyev <1viklover@gmail.com>
2024-01-14 23:18:21 +0500

    [FEATURE] configure DI for application repositories

---
+++ Repository/RepositoriesRegistration.cs
@@ -0,0 +1,15 @@
+using Core.Storage.Impl.SplayTree.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Repository;
+
+using Storage;
+
+public static class RepositoriesRegistration
+{
+    public static IServiceCollection ConfigureRepositories(this IServiceCollection services)
+    {
+        services.AddSingleton<IStorageRepository, StorageRepository>();
+        return services;
+    }
+}
