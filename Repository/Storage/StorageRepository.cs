060ca83ac7d2373465d1957cad64b5c1116287ea
Mihail Vorobyev <1viklover@gmail.com>
2024-01-14 23:12:01 +0500

    [FEATURE] Make StorageRepository object implement required interface

--- Repository/Storage/StorageRepository.cs
+++ Repository/Storage/StorageRepository.cs
@@ -1,8 +1,9 @@
 using Cassandra;
+using Core.Storage.Impl.SplayTree.Infrastructure;
 
 namespace Repository.Storage;
 
-public class StorageRepository
+public class StorageRepository : IStorageRepository
 {
     private readonly Cluster _cluster = Cluster.Builder()
         .AddContactPoint(Environment.GetEnvironmentVariable("CLUSTER_URL"))
@@ -10,6 +11,11 @@ public class StorageRepository
 
     public ISession Session() => _cluster.Connect("storage");
 
+    public void PrepareSchemas()
+    {
+        throw new NotImplementedException();
+    }
+    
     ~StorageRepository()
     {
         _cluster.Dispose();
